9e79be8bb31d47169e3de1e645a765ac13f831c2
Brad Garropy <bradgarropy@gmail.com>
2018-12-20 00:09:06 -0600

    global styles.

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,11 +1,42 @@
 import React from "react"
 import PropTypes from "prop-types"
 import {ThemeProvider} from "styled-components"
+import {createGlobalStyle} from "styled-components"
 import Meta from "./Meta"
 import Header from "./Header"
 import theme from "./styles/theme"
 import StyledPage from "./styles/StyledPage"
 import Container from "./styles/Container"
+
+const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'radnika_next';
+        src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+    html {
+        box-sizing: border-box;
+        font-size: 10px;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+    body {
+        padding: 0;
+        margin: 0;
+        font-size: 1.5rem;
+        line-height: 2;
+        font-family: 'radnika_next';
+    }
+    a {
+        text-decoration: none;
+        color: ${props => props.theme.black};
+    }
+    button {
+        font-family: 'radnika_next';
+    }
+`
 
 class Page extends React.Component {
     static propTypes = {children: PropTypes.node}
@@ -15,11 +46,14 @@ class Page extends React.Component {
     render = () => {
         return (
             <ThemeProvider theme={theme}>
-                <StyledPage>
-                    <Meta/>
-                    <Header/>
-                    <Container>{this.props.children}</Container>
-                </StyledPage>
+                <React.Fragment>
+                    <GlobalStyle/>
+                    <StyledPage>
+                        <Meta/>
+                        <Header/>
+                        <Container>{this.props.children}</Container>
+                    </StyledPage>
+                </React.Fragment>
             </ThemeProvider>
         )
     }
