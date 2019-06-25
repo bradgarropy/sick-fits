033f0c19f5cd91deb84ed6b96fb034f378ba94c5
Brad Garropy <bradgarropy@gmail.com>
2019-06-25 10:11:37 -0500

    fresh.

--- frontend/components/styles/Container.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-const Container = styled.div`
-    max-width: ${props => props.theme.maxWidth};
-    margin: 0 auto;
-    padding: 2rem;
-`
-
-export default Container
--- frontend/components/styles/StyledPage.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-const StyledPage = styled.div`
-    background: white;
-    color: ${props => props.theme.black};
-`
-
-export default StyledPage
