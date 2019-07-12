fb4ab70b282a2e789b50b0996e86bc7961a706c9
Brad Garropy <bradgarropy@gmail.com>
2019-07-12 00:01:45 -0500

    signup mutation.

--- backend/graphql/mutation.js
+++ backend/graphql/mutation.js
@@ -1,13 +1,31 @@
 const database = require("../prisma/database")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const Mutation = {
     createUser: (parent, {data}) => database.mutation.createUser({data}),
     createItem: (parent, {data}) => database.mutation.createItem({data}),
     updateItem: (parent, {data, where}) => database.mutation.updateItem({data, where}),
     deleteItem: (parent, {where}) => database.mutation.deleteItem({where}),
-    // signup: (parent, args) => {
-    //     console.log(args)
-    // },
+    signup: async (parent, args, context) => {
+        args.email = args.email.toLowerCase()
+        args.password = await bcrypt.hash(args.password, 10)
+
+        const user = await database.mutation.createUser({
+            data: {
+                ...args,
+                permissions: {set: ["USER"]}
+            }
+        })
+
+        const token = jwt.sign({id: user.id}, process.env.SECRET)
+        context.res.cookie("token", token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365,
+        })
+
+        return user
+    },
 }
 
 module.exports = Mutation
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 require("dotenv").config()
+const cookieParser = require("cookie-parser")
 const server = require("./graphql/server")
+
+server.use(cookieParser())
 
 server.listen({port: process.env.PORT}, () => {
     console.log(`🚀  Server ready at http://localhost:${process.env.PORT}/graphql`)
