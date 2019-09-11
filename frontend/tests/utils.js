92da09a492efd2a02db3e8c5b92a4e335d121e1f
Brad Garropy <bradgarropy@gmail.com>
2019-09-10 23:04:24 -0500

    mock data.

---
+++ frontend/tests/utils.js
@@ -0,0 +1,20 @@
+const mockItem = {
+    id: "1234",
+    title: "Cool Item",
+    description: "It's really cool.",
+    price: 5000,
+    image: "cool.jpg",
+    largeImage: "large-cool.jpg",
+    user: null,
+}
+
+const mockUser = {
+    id: "asdf",
+    name: "Brad Garropy",
+    email: "bradgarropy@gmail.com",
+    permissions: ["ADMIN"],
+    orders: [],
+    cart: [],
+}
+
+export {mockItem, mockUser}
